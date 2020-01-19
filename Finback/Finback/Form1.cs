249d336c88e1d1e60bd046211ba24484c5359a36
TIK <firmament_solo@hotmail.com>
2020-01-19 20:27:02 +0800

    T.I. created files

---
+++ Finback/Finback/Form1.cs
@@ -0,0 +1,39 @@
+﻿//JAN 19 2020 T.I. Created this file
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Finback
+{
+    public partial class Form1 : Form
+    {
+
+ 
+        perfDrawing x1=new perfDrawing();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = x1.draw24x30();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Finback/Finback/perfDrawing.cs
@@ -0,0 +1,62 @@
+﻿//JAN 19 2020 T.I. Created this file
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Finback
+{
+    class perfDrawing
+    {
+
+        public perfDrawing()
+        {
+
+
+        }
+        public Bitmap drawingTest()
+        {
+            Bitmap DrawArea;
+            Graphics g;
+            DrawArea = new Bitmap(300, 300);
+
+
+            g = Graphics.FromImage(DrawArea);
+
+            Pen mypen = new Pen(Color.Black);
+
+            g.DrawLine(mypen, 0, 0, 200, 150);
+
+            return DrawArea;
+        }
+        public Bitmap draw24x30(int scale=10)
+        {
+            Bitmap DrawArea;
+            Graphics g;
+            DrawArea = new Bitmap(400, 400);
+            int GridStartX = 0, GridStartY = 0;
+            int GridEndX = 1* scale, GridEndY = 1* scale;
+            int i = 0,j=0;
+
+            g = Graphics.FromImage(DrawArea);
+
+            Pen mypen = new Pen(Color.Black);
+
+            for(i=0;i<30;i++)
+            {
+                for(j = 0; j < 24; j++)
+                {
+                    g.DrawRectangle(mypen, GridStartX, GridStartY, GridEndX, GridEndY);
+                    GridStartX = GridStartX + GridEndX;
+                }
+                GridStartY = GridStartY+ GridEndY;
+                GridStartX = 0;
+            }
+            
+
+            return DrawArea;
+        }
+    }
+}
